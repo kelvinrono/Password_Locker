f66cc6e079e54a11256e333751c9f6dadd098fc8
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-29 11:22:06 +0300

    Take the user details and store them in the system

--- run.py
+++ run.py
@@ -12,4 +12,37 @@ def save_user(user):
     '''
     Function that will save the user
     '''
-    user.save_user()
+def display_user():
+    return User.display_user()
+
+def main():
+    print('Welcome to password ;Locker app,please enter your name \n')
+    user_name = input()
+    print(f"Hello {user_name}. please create password locker acount first")
+    print("\n")
+
+    while True:
+        print("Use the following codes to proceed:\n na - create a new contact,\n dc - display contact,\n ex - exit the contact list")
+        short_code = input().lower()
+
+        if short_code == 'na':
+            print("New Account")
+            print("-"*30)
+            
+            print("Enter your username : ")
+            userName = input()
+            
+            print("Enter your first name : ")
+            first_name = input()
+
+            print("Enter the last name : ")
+            last_name = input()
+
+            print("Enter your password : ")
+            password = input()
+
+            
+
+main()
+    
+
