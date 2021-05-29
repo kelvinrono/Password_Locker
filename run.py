43f75e1676aff5ab7c9bffc1ad6c5aca06619c40
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-05-29 10:40:43 +0300

    defined a function to save the user

--- run.py
+++ run.py
@@ -2,4 +2,14 @@
 from user import User
 
 def createUserAccount(firstName, lastName, username, password):
+    '''
+    This function will create a new user account
+    '''
     new_user = User(firstName, lastName, username,password)
+    return new_user
+
+def save_user(user):
+    '''
+    Function that will save the user
+    '''
+    user.save_user()
